cfb1996f83cf523687cfc07fd3828f3fe0ab208c
AltyFox <bartat404@gmail.com>
2025-04-21 13:06:23 -0500

    v1.1.3 Reorder the text so important messages are less likely to be missed at the end

--- debug.ps1
+++ debug.ps1
@@ -1,10 +1,10 @@
 # Delete test.exe if it exists
-if (Test-Path "test.exe") {
-    Remove-Item -Path "test.exe" -Force
+if (Test-Path "debug.exe") {
+    Remove-Item -Path "debug.exe" -Force
 }
 
 # Convert install.ps1 to test.exe using ps2exe
-ps2exe "install.ps1" "test.exe" -noConsole
+ps2exe "install.ps1" "debug.exe" -noConsole
 
 # Run the generated test.exe
-Start-Process -FilePath "test.exe" -Wait
+Start-Process -FilePath "debug.exe" -Wait
